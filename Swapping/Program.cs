27a0ae8703d1b9f5875030b6cb6d6a888d574db8
akshbedarkar <114993159+akshbedarkar@users.noreply.github.com>
2022-10-20 18:45:20 +0530

    swapping number
    assignment 1.4

---
+++ Swapping/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Swapping
+{
+    public class Program
+    {
+
+        public static void swap(int number1,int number2 )
+        {
+            int temp = 0;
+
+            temp = number2;
+            number2 = number1;
+            number1 = temp;
+
+            Console.WriteLine($"the swapped values are number1:{number1} and number2 :{number2}");
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Console.WriteLine("Enter the numbers you want to swap");
+            int number1=int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter 1 if you want to perform swapping operation");
+            int option = int.Parse(Console.ReadLine());
+
+
+            if (option !=1)
+            {
+                return;
+            }
+            else
+            {
+                swap(number1 ,number2);
+            }
+
+        }
+    }
+}
