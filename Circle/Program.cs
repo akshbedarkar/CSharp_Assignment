b9caf303447d5d8ca4b749588eb209994f4c3825
akshbedarkar <114993159+akshbedarkar@users.noreply.github.com>
2022-10-20 19:03:21 +0530

    Circumference and Area of Circle
    Assignment 1.5

---
+++ Circle/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Circle
+{
+    public class Program
+    {
+        const float PI= 3.14f;
+        public static void Calculate(float radius)
+        {
+            float area = (PI * (radius * radius));
+            float circumference = 2 * PI * radius;
+
+            Console.WriteLine($"For radius {radius} the circumference is {circumference:N1} and area is {area:N1}");
+
+        }
+        static void Main(string[] args)
+        {
+            Program p=new Program();
+            Console.WriteLine("Enter the radius");
+            float radius=float.Parse(Console.ReadLine());
+
+            Calculate(radius);
+            
+
+        }
+    }
+}
