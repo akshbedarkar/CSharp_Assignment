9cb08f62d7a4044055b7d07310a67be8ec8c0305
akshbedarkar <114993159+akshbedarkar@users.noreply.github.com>
2022-10-23 20:06:20 +0530

    Basic File Operation
    Added a BadicFileOperation.txt with all details.
    
    Assignment 7.1

---
+++ FileIO/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System.IO;
+using System.IO;
+
+namespace FileIO
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string path = @"C:\Users\akshata\source\fieio\MyDirectory\MyAnotherDirectory\NewFile.txt";
+            using(FileStream fs=new FileStream(path,FileMode.Open,FileAccess.Read))
+            {
+                using (StreamReader sr = new StreamReader(fs)) 
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+                
+            }
+
+            
+
+
+
+
+
+
+        }
+    }
+}
