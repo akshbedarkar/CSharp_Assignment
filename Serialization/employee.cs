78a4ae0567eef7f4786048803c7f26b4e168d426
akshbedarkar <114993159+akshbedarkar@users.noreply.github.com>
2022-10-24 07:11:35 +0530

    Deserialization of employee class
    Assignment 7.3,7.4

---
+++ Serialization/employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization
+{
+    [Serializable]
+    internal class employee
+    {
+        public int id;
+        public string name;
+        public int basicsalary;
+        public employee(int id, string name, int basicsalary)
+        {
+            this.id = id;
+            this.name = name;
+            this.basicsalary = basicsalary;
+        }
+
+
+
+    }
+}
