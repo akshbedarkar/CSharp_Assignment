11b989583beb32cbc5d53394e67a521a7326b1ba
akshbedarkar <114993159+akshbedarkar@users.noreply.github.com>
2022-10-24 11:54:42 +0530

    reflection
    Assignment 8.1

---
+++ UserData/Class1.cs
@@ -0,0 +1,17 @@
+﻿namespace UserData
+{
+    public class Userdata
+    {
+        private string name;
+
+
+
+        public string displayname(string name)
+        {
+            return name;
+        }
+
+
+
+    }
+}
